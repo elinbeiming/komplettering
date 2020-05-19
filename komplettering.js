e4335f460a0aa1383dafb6b8021e93627571dda5
elinbeiming <46751847+elinbeiming@users.noreply.github.com>
2020-05-19 15:35:12 +0200

    stuff

--- komplettering.js
+++ komplettering.js
@@ -1,10 +1,10 @@
-const http = require('http');
-const os = require('os');
+var http = require('http');
+var os = require('os');
 
-var server = http.createServer(function functionNamere(request, respons) {
+var server = http.createServer(function (request, respons) {
   respons.writeHead(200, {"Content-type": "text/html"});
-  respons.write("Hello");
+  respons.write(os.hostname());
   respons.end();
 })
 
-app.listen(8000);
+app.listen(3000);
